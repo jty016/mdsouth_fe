7607939b2633119a805aa642f3558e5c951ad7b7
Lucas Jo <jty016@gmail.com>
2022-05-08 15:54:24 +0000

    typiong errors are adjusted

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,14 @@ module.exports = {
     'airbnb',
     'plugin:import/errors',
     'plugin:import/warnings',
-    "prettier",
+    'prettier',
     'plugin:prettier/recommended',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   // parserOptions: {
   //   project: "./tsconfig.json",
   //   ecmaVersion: 2018,
@@ -34,8 +34,13 @@ module.exports = {
     'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'no-shadow': 0,
     'react/prop-types': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/require-default-props': 'off',
   },
-  ignorePatterns: ["dist/", "node_modules/"],
+  ignorePatterns: ['dist/', 'node_modules/'],
 };
